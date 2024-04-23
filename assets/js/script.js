9cb9e4ec5d0f5bdcf028e60e0a9588b7bd495ecc
Jhonny Silva <matias.silva.200517@gmail.com>
2024-04-23 12:37:06 -0400

    commita

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,51 @@ console.log(`El ides: ${id}`);
 console.log(`El nombre es: ${name}`);
 
  
+
+///funcion FIND/////-
+const getRobotById =(id='013')=>{
+    return robots.find((robot)=>{
+        return robot.id === id;
+    });
+
+}
+let robot=getRobotById("001")
+console.log(robot);
+
+
+//FUNCION FILTER trae mas de 1 dato///
+
+const getRobotBySeries=(series=1)=>{
+    return robots.filter((robot)=>{
+        return robot.series===series;
+    });
+}
+
+robot= getRobotBySeries(1);
+console.table(robot);
+
+
+//FUNCION map recorre el arreglo per devolver un arreglo nuevo
+
+const createCard= (series,id,name,weapon,avatar,sprite1)=>{
+    return`
+    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+        <img src="${avatar}" class="card-img-top" alt="${name}">
+        <img src="${sprite1}" class="card-img-top" alt="${name}">
+        <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p>${series}</p>
+        <p>${weapon}</p>
+        </div>
+    
+    </div>
+    </div>
+    `;
+}
+
+const robotCardsRow=document.getElementById('robotCardsRow');
+    robots.map ((robot)=>{
+        const card=createCard(robot.series,robot.id,robot.name,robot.weapon,robot.avatar,robot.sprite1);
+        robotCardsRow.innerHTML += card;
+    });
