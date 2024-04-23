5961f0a39472f24bd6a50e1d25adb32ab19558b2
Jhonny Silva <matias.silva.200517@gmail.com>
2024-04-23 12:36:51 -0400

    commita

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+import { robots } from "./robots.js";
+
+console.log(robots[0].series);
+console.log(robots[0].id);
+console.log(robots[0].name);
+
+
+const{series, id, name}= robots[0];
+
+
+console.log("-----------------");
+
+console.log(`La serie es: ${series}`);
+console.log(`El ides: ${id}`);
+console.log(`El nombre es: ${name}`);
+
+ 
